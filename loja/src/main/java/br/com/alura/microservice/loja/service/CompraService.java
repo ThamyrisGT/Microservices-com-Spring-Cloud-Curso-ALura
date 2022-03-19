7526d9e680fd9b23eaa9b94aa0a661ede85231d5
Thamyris <tgthurler@gmail.com>
2022-03-18 23:51:53 -0300

    Implementado o service Eureka, para que os microsserviços possam se conhecer sem utilizar o hard code, e o LoadBalanced para conseguir fazer as requisições

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.alura.microservice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,12 +12,13 @@ import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
 @Service
 public class CompraService {
 
+	@Autowired
+	private RestTemplate client;
+	
 	public void realizaCompra(CompraDTO compra) {
-
-		RestTemplate client = new RestTemplate(); // permite fazer requisições do tipo get, post, etc, através de métodos
 													
 		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
-				"http://localhost:8081/info/"+compra.getEndereco().getEstado(), HttpMethod.GET, null,
+				"http://fornecedor/info/"+compra.getEndereco().getEstado(), HttpMethod.GET, null,
 				InfoFornecedorDTO.class);
 		
 		System.out.println(exchange.getBody().getEndereco());
